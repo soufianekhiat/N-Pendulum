2fb505d3a5e2f2c6a0e7693d570699079141751c
soufianekhiat <soufiane.khiat@gmail.com>
2020-05-19 05:25:59 +0200

    Add 2 integrators (wrong)

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,47 @@ typedef	long long				Int64;
 typedef	float					f32;
 typedef	double					f64;
 
+inline Bool		operator==(const ImVec2& lhs, const ImVec2& rhs){ return lhs.x == rhs.x && lhs.y == rhs.y; }
+inline Bool		operator!=(const ImVec2& lhs, const ImVec2& rhs){ return lhs.x != rhs.x || lhs.y != rhs.y; }
+inline ImVec2	operator+(const ImVec2& lhs)					{ return ImVec2( lhs.x,  lhs.y); }
+inline ImVec2	operator-(const ImVec2& lhs)					{ return ImVec2(-lhs.x, -lhs.y); }
+//inline ImVec2	operator+(const ImVec2& lhs, const ImVec2& rhs)	{ return ImVec2(lhs.x+rhs.x, lhs.y+rhs.y); }
+//inline ImVec2	operator-(const ImVec2& lhs, const ImVec2& rhs)	{ return ImVec2(lhs.x-rhs.x, lhs.y-rhs.y); }
+//inline ImVec2	operator*(const ImVec2& lhs, float rhs)			{ return ImVec2(lhs.x * rhs,   lhs.y * rhs); }
+inline ImVec2	operator*(float lhs, const ImVec2& rhs)			{ return ImVec2(lhs * rhs.x, lhs * rhs.y); }
+
+enum Integrator
+{
+	VerletPosition = 0,
+	VerletVelocity
+};
+
+inline
+f32 Dot(ImVec2 a, ImVec2 b)
+{
+	return a.x*b.x + a.y*b.y;
+}
+
+inline
+f32 Norm2Sqr(ImVec2 v)
+{
+	return Dot(v, v);
+}
+
+inline
+f32 Norm2(ImVec2 v)
+{
+	return std::sqrtf(Dot(v, v));
+}
+
+inline
+ImVec2 Normalized(ImVec2 v)
+{
+	f32 const fInvNorm = 1.0f/Norm2(v);
+
+	return v*fInvNorm;
+}
+
 static
 void GLFWErrorCallback(int error, const char* description)
 {
@@ -61,10 +102,10 @@ ImFont* ImGui_LoadFont(ImFontAtlas& oAtlas, char const* name, f32 const fSize, I
 	static const ImWchar ranges[] =
 	{
 		0x0020, 0x00FF, // Basic Latin + Latin Supplement
-		0x0104, 0x017C, // Polish characters and more
+		//0x0104, 0x017C, // Polish characters and more
+		0x0100, 0x0500, // Polish characters and more
 		0,
 	};
-
 	ImFontConfig config;
 	config.OversampleH = 4;
 	config.OversampleV = 4;
@@ -176,6 +217,155 @@ void	InitializeImGUIStyle()
 	}
 }
 
+std::vector< ImVec2 >	oPendulumPos;
+std::vector< ImVec2 >	oPendulumPosPrev;
+std::vector< ImVec2 >	oPendulumVel;
+std::vector< ImVec2 >	oPendulumVelPrev;
+std::vector< ImVec2 >	oPendulumAcc;
+std::vector< ImVec2 >	oPendulumAccPrev;
+std::vector< f32 >		oPendulumMass;
+
+f32						fG			= 9.81f;
+f32						fDt			= 0.0125f;
+Int32					iIter		= 32;
+Int32					iLinksCount	= 5;
+Bool					bSimulate	= false;
+
+void	Constraints()
+{
+	for ( Int32 iIdx = 0; iIdx < iIter; ++iIdx )
+	{
+		for ( Int32 iLinkIdx = 1; iLinkIdx <= iLinksCount; ++iLinkIdx )
+		{
+			ImVec2& vA = oPendulumPos[ iLinkIdx - 1 ];
+			ImVec2& vB = oPendulumPos[ iLinkIdx - 0 ];
+
+			ImVec2	const	vDelta	= vB - vA;
+			f32		const	fNorm2	= Norm2( vDelta );
+			f32		const	fDelta	= fNorm2 - 1.0f;
+			ImVec2	const	vDir	= Normalized( vDelta );
+
+			vA = vA + vDir*fDelta*0.5f;
+			vB = vB - vDir*fDelta*0.5f;
+		}
+		oPendulumPos[ 0 ] = ImVec2( 0, 0 );
+	}
+}
+
+void	SimulateStormerVerlet()
+{
+	auto oItPrev	= oPendulumPosPrev.begin();
+	auto oItAcc		= oPendulumAcc.begin();
+	auto oItMass	= oPendulumMass.begin();
+
+	f32 const fDt2 = fDt*fDt;
+
+	for ( ImVec2& v : oPendulumPos )
+	{
+		f32 const m		= *oItMass;
+		f32 const ax	= m > 0.0f ? oItAcc->x/m : 0.0f;
+		f32 const ay	= m > 0.0f ? oItAcc->y/m : 0.0f;
+		// Verlet Integration
+		v.x = 2.0f*v.x - oItPrev->x + ax*fDt2;
+		v.y = 2.0f*v.y - oItPrev->y + ay*fDt2;
+
+		++oItPrev;
+		++oItAcc;
+		++oItMass;
+	}
+
+	Constraints();
+}
+
+void	SimulateLeapfrog()
+{
+	auto oItPrev	= oPendulumPosPrev.begin();
+	auto oItVel		= oPendulumVel.begin();
+	auto oItAcc		= oPendulumAcc.begin();
+	auto oItMass	= oPendulumMass.begin();
+
+	f32 const fDt2 = fDt*fDt;
+
+	for ( ImVec2& v : oPendulumPos )
+	{
+		f32 const m		= *oItMass;
+		f32 const ax	= m > 0.0f ? oItAcc->x/m : 0.0f;
+		f32 const ay	= m > 0.0f ? oItAcc->y/m : 0.0f;
+		// Verlet Integration
+		v.x += oItVel->x*fDt + ax*fDt2*0.5f;
+		v.y += oItVel->y*fDt + ay*fDt2*0.5f;
+
+		oItVel->x += ax*fDt;
+		oItVel->y += ay*fDt;
+
+		++oItPrev;
+		++oItVel;
+		++oItAcc;
+		++oItMass;
+	}
+
+	Constraints();
+}
+
+Int32 iSelectedIntegrator = 0;
+typedef void ( *pfSimPtr )();
+std::vector< pfSimPtr >		oIntegrator			= { &SimulateStormerVerlet, &SimulateLeapfrog };
+std::vector< std::string >	oIntegratorNames	= { "Stormer-Verlet (Position Verlet)", "Leapfrog (Velocity Verlet)" };
+pfSimPtr					pfCurrentIntegrator = &SimulateStormerVerlet;
+
+void	Simulate()
+{
+	std::vector< ImVec2 > oPosCopy = oPendulumPos;
+	std::vector< ImVec2 > oVelCopy = oPendulumVel;
+	std::vector< ImVec2 > oAccCopy = oPendulumAcc;
+
+	( *pfCurrentIntegrator )();
+
+	oPendulumPosPrev = oPosCopy;
+	oPendulumVelPrev = oVelCopy;
+	oPendulumAccPrev = oAccCopy;
+}
+
+void	Reset()
+{
+	oPendulumPos.clear();
+	oPendulumPos.reserve( iLinksCount + 1 );
+
+	oPendulumVel.clear();
+	oPendulumVel.reserve( iLinksCount + 1 );
+
+	oPendulumAcc.clear();
+	oPendulumAcc.reserve( iLinksCount + 1 );
+
+	oPendulumMass.clear();
+	oPendulumMass.reserve( iLinksCount + 1 );
+
+	oPendulumPos.emplace_back( 0.0f, 0.0f );
+	oPendulumVel.emplace_back( 0.0f, 0.0f );
+
+	oPendulumMass.push_back( 0.0f );
+
+	f32 fPosX = 0.0f;
+	f32 fPosY = 0.0f;
+	for ( Int32 iIdx = 0; iIdx < iLinksCount; ++iIdx )
+	{
+		fPosX += 1.0f; // Link length
+		fPosY += 0.0f;
+
+		oPendulumPos.emplace_back( fPosX, fPosY );
+		oPendulumVel.emplace_back( 0.0f, 0.0f );
+		oPendulumMass.push_back( f32( iLinksCount - iIdx ) );
+	}
+
+	oPendulumAcc.resize( iLinksCount + 1 );
+	std::fill( oPendulumAcc.begin(), oPendulumAcc.end(), ImVec2( 0.0f, fG ) );
+	oPendulumAcc[ 0 ] = ImVec2( 0.0f, 0.0f );
+
+	oPendulumPosPrev = oPendulumPos;
+	oPendulumVelPrev = oPendulumVel;
+	oPendulumAccPrev = oPendulumAcc;
+}
+
 int main()
 {
 	glfwSetErrorCallback( GLFWErrorCallback );
@@ -194,7 +384,7 @@ int main()
 		return 1;
 
 	glfwMakeContextCurrent( pWindow );
-	glfwSwapInterval( 1 ); // Enable vsync
+	//glfwSwapInterval( 1 ); // Enable vsync
 
 	Bool bErr = gladLoadGL() == 0;
 	if ( bErr )
@@ -221,8 +411,22 @@ int main()
 	ImGui_ImplGlfw_InitForOpenGL( pWindow, true );
 	ImGui_ImplOpenGL3_Init( glsl_version );
 
-	ImVec4 vClearColor = ImVec4( 0.45f, 0.55f, 0.60f, 1.00f );
+	ImVec4 const vOrange	= ImVec4(255.f/255.f,	128.f/255.f,  0.f,			1.0f);
+	ImVec4 const vBlue		= ImVec4(  0.f/255.f,	128.f/255.f, 255.f/255.f,	1.0f);
+	ImVec4 const vGreen		= ImVec4( 64.f/255.f,	192.f/255.f,  64.f/255.f,	1.0f);
+	ImVec4 const vPurple	= ImVec4(1.f - 64.f/255.f, 1.0f - 192.f/255.f, 1.0f - 64.f/255.f, 1.0f);
 
+	Reset();
+
+	ImGuiDockNodeFlags	eDockspaceFlags = ImGuiDockNodeFlags_None;
+	ImGuiWindowFlags	eWindowFlags	=	ImGuiWindowFlags_MenuBar |
+											ImGuiWindowFlags_NoDocking |
+											ImGuiWindowFlags_AlwaysAutoResize;
+
+	eWindowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+	eWindowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+	ImVec4 vClearColor = ImVec4( 0.45f, 0.55f, 0.60f, 1.00f );
 	while ( !glfwWindowShouldClose( pWindow ) )
 	{
 		glfwPollEvents();
@@ -232,32 +436,106 @@ int main()
 		ImGui::NewFrame();
 
 		{
-			ImGuiDockNodeFlags	dockspace_flags = ImGuiDockNodeFlags_None;
-			ImGuiWindowFlags	eWindowFlags	=	ImGuiWindowFlags_MenuBar |
-													ImGuiWindowFlags_NoDocking |
-													ImGuiWindowFlags_AlwaysAutoResize;
-
 			ImGuiViewport* viewport = ImGui::GetMainViewport();
 			ImGui::SetNextWindowPos		( viewport->Pos );
 			ImGui::SetNextWindowSize	( viewport->Size );
 			ImGui::SetNextWindowViewport( viewport->ID );
 
-			eWindowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-			eWindowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
 			Bool bOpen = true;
 			ImGui::Begin( "Dockspace", &bOpen, eWindowFlags );
 
 			ImGuiID uDockspaceID = ImGui::GetID( "_DockSpace" );
-			ImGui::DockSpace( uDockspaceID, ImVec2( 0.0f, 0.0f ), dockspace_flags );
+			ImGui::DockSpace( uDockspaceID, ImVec2( 0.0f, 0.0f ), eDockspaceFlags );
 
 			ImGui::Begin( "Infos" );
+				if ( ImGui::SliderInt( "Links Count", &iLinksCount, 1, 15 ) )
+				{
+					Reset();
+				}
+				if ( ImGui::Button( bSimulate ? "Stop" : "Simulate" ) )
+				{
+					bSimulate = !bSimulate;
+				}
+				if ( ImGui::Button( "Reset" ) )
+				{
+					Reset();
+				}
+				ImGui::InputFloat( "Gravity", &fG, 1.0f, 0.0f );
+				ImGui::InputFloat( ( char const* )u8"\u0394t", &fDt, 1.0f, 0.0f );
+				if ( ImGui::InputInt( "Iteration", &iIter ) )
+				{
+					iIter = std::max( iIter, 1 );
+				}
+				std::string sItem = oIntegratorNames[ iSelectedIntegrator ];
+				if ( ImGui::BeginCombo( "Integrator", sItem.c_str(), 0 ) )
+				{
+					Int32 iIdx = 0;
+					Int32 iNewIdx = -1;
+					for ( std::string const& sIntegrator : oIntegratorNames )
+					{
+						Bool bIsSelected = ( sItem == sIntegrator );
+						if ( ImGui::Selectable( sIntegrator.c_str(), bIsSelected ) )
+						{
+							sItem = sIntegrator;
+							iNewIdx = iIdx;
+						}
+						if ( bIsSelected )
+							ImGui::SetItemDefaultFocus();
+						++iIdx;
+					}
+					ImGui::EndCombo();
+					if ( iNewIdx >= 0)
+					{
+						pfCurrentIntegrator = oIntegrator[ iNewIdx ];
+						iSelectedIntegrator = iNewIdx;
+					}
+				}
 			ImGui::End();
 
 			ImGui::Begin( "Draw" );
+				ImDrawList*	pDrawList	= ImGui::GetWindowDrawList();
+				ImVec2		vPos		= ImGui::GetCursorScreenPos();
+				ImVec2		vSize		= ImGui::GetWindowContentRegionMax();
+				ImVec2		vCenter		= ImVec2( vPos.x + vSize.x*0.5f, vPos.y + vSize.y*0.5f );
+
+				constexpr f32 fSecurityZone	= 0.05f; // 10%
+				f32 const fMinSize	= std::min( vSize.x, vSize.y )*( 1.0f - fSecurityZone );
+				f32 const fLinkSize	= 0.5f*fMinSize/f32( iLinksCount + 1 );
+
+				std::vector< ImVec2 > oVizPendulum;
+
+				oVizPendulum.clear();
+				oVizPendulum.reserve( iLinksCount + 1 );
+
+				f32 fPosX = vPos.x + vSize.x*0.5f;
+				f32 fPosY = vPos.y + vSize.y*0.5f;
+
+				oVizPendulum.emplace_back( fPosX, fPosY );
+
+				for ( Int32 iIdx = 0; iIdx < iLinksCount; ++iIdx )
+				{
+					fPosX = vCenter.x + oPendulumPos[ iIdx ].x*fLinkSize;
+					fPosY = vCenter.y + oPendulumPos[ iIdx ].y*fLinkSize;
+
+					oVizPendulum.emplace_back( fPosX, fPosY );
+				}
+				pDrawList->AddPolyline( oVizPendulum.data(), int( oVizPendulum.size() ), ImGui::GetColorU32( vBlue ), false, 8.0f );
+
+				for ( Int32 iIdx = 1; iIdx <= iLinksCount; ++iIdx )
+				{
+					f32 const m = oPendulumMass[ iIdx ];
+					f32 const r = 8.0f*m/f32( iLinksCount );
+
+					pDrawList->AddCircleFilled( oVizPendulum[ iIdx ], r, ImGui::GetColorU32( vOrange ), 16 );
+				}
 			ImGui::End();
 
 			ImGui::End();
+
+			if ( bSimulate )
+			{
+				Simulate();
+			}
 		}
 
 		ImGui::Render();
